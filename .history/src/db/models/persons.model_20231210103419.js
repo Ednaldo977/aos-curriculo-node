63950cfd730ffc64272d265633bf4f4a7fb777c0
Ednaldo Junior <carlosednaldo97@gmail.com>
2023-12-10 10:37:22 -0300

    persons.models

---
+++ .history/src/db/models/persons.model_20231210103419.js
@@ -0,0 +1,46 @@
+const { Model, DataTypes, Sequelize } = require('sequelize');
+
+const PERSON_TABLE = 'persons';
+
+class Person extends Model {
+    static config(sequelize) {
+        return {
+            sequelize,
+            tableName: PERSON_TABLE,
+            modelName: 'Person',
+            timestamps: true
+        }
+    }
+} 
+
+const PersonSchema = {
+    id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER
+    },
+    name: {
+        allowNull: false,
+        type: DataTypes.STRING,
+        field:'name'
+    },
+    address:{ 
+        allowNull:false,
+        type: DataTypes.STRING,
+        field: 'address'
+    },
+    
+    createdAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+        field: 'createdAt'
+    },
+    updatedAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+        field: 'updatedAt'
+    }
+}
+  
+module.exports = { Person, PersonSchema };
